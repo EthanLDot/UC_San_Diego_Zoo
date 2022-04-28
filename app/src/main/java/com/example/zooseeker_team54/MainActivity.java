f31afc1d4edbfa0d66050a5ca681698009edfe30
EthanLDot <ethanlee2337@gmail.com>
2022-04-28 15:35:44 -0700

    #2: Exhibits Planned UI

--- app/src/main/java/com/example/zooseeker_team54/MainActivity.java
+++ app/src/main/java/com/example/zooseeker_team54/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.zooseeker_team54;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<Place> todos = Place.loadJSON(this, "sample_node_info.json");
-        System.out.println(todos.toString());
-        System.out.println("oncreate complete!");
+        List<Place> places = Place.loadJSON(this, "sample_node_info.json");
+        List<String> exhibits = new ArrayList<>();
+        for(Place place: places)
+        {
+            if(place.kind.equals("exhibit"))
+            {
+                exhibits.add(place.id);
+            }
+        }
+        selectedExhibitComponent sec = new selectedExhibitComponent(exhibits, this);
+        sec.display();
     }
 }
--- app/src/main/java/com/example/zooseeker_team54/selectedExhibitComponent.java
+++ app/src/main/java/com/example/zooseeker_team54/selectedExhibitComponent.java
@@ -1,22 +1,34 @@
 package com.example.zooseeker_team54;
 
-import java.util.HashSet;
+import android.app.Activity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class selectedExhibitComponent {
 
-    HashSet<String> plan;
+    List<String> plan;
+    private static final int PLANSIZE = R.id.planSize;
+    Activity activity;
 
     //assuming that a hashset of ids will be passed
-    public selectedExhibitComponent(HashSet<String> plan)
+    public selectedExhibitComponent(List<String> plan, Activity activity)
     {
         this.plan = plan;
+        this.activity = activity;
     }
 
     public void display()
     {
+        Log.d("selectedExhibitComponent", "" + plan.size());
         for(String s: plan)
         {
-            System.out.println(s);
+            Log.d("selectedExhibitComponent", s);
         }
+        int size = plan.size();
+        TextView plan_size =(TextView) activity.findViewById(PLANSIZE);
+        plan_size.setText("" + size);
+
     }
 }

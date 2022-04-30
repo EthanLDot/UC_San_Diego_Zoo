e33e72397d4596943e21ac5f0bc64a5b0c15991e
EthanLDot <ethanlee2337@gmail.com>
2022-04-29 21:14:50 -0700

    integrated remaining assets and given java code

--- app/src/main/java/com/example/zooseeker_team54/MainActivity.java
+++ app/src/main/java/com/example/zooseeker_team54/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.zooseeker_team54;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +20,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         List<Place> places = Place.loadJSON(this, "sample_node_info.json");
         List<String> exhibits = new ArrayList<>();
-        for(Place place: places)
-        {
-            if(place.kind.equals("exhibit"))
-            {
+        for (Place place : places) {
+            if (place.kind.equals("exhibit")) {
                 exhibits.add(place.id);
             }
         }
         selectedExhibitComponent sec = new selectedExhibitComponent(exhibits, this);
         sec.display();
+
+        // "source" and "sink" are graph terms for the start and end
+        String start = "entrance_exit_gate";
+        String goal = "elephant_odyssey";
+
+        // 1. Load the graph...
+        Graph<String, IdentifiedWeightedEdge> g = ZooData.loadZooGraphJSON("assets/sample_zoo_graph.json");
+        GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g, start, goal);
+
+        // 2. Load the information about our nodes and edges...
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON("assets/sample_node_info.json");
+        Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON("assets/sample_edge_info.json");
+
+        System.out.printf("The shortest path from '%s' to '%s' is:\n", start, goal);
+
+        int i = 1;
+        for (IdentifiedWeightedEdge e : path.getEdgeList()) {
+            System.out.printf("  %d. Walk %.0f meters along %s from '%s' to '%s'.\n",
+                    i,
+                    g.getEdgeWeight(e),
+                    eInfo.get(e.getId()).street,
+                    vInfo.get(g.getEdgeSource(e).toString()).name,
+                    vInfo.get(g.getEdgeTarget(e).toString()).name);
+            i++;
+        }
     }
 }

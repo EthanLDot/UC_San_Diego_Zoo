11a15e4e6d9612a1cf9830f4ed39a819100a771e
James Zhang <jjz002@ucsd.edu>
2022-05-28 19:46:42 -0700

    Add tests for User Story 2

--- app/src/test/java/com/example/zooseeker_team54/MockLocationTest.java
+++ app/src/test/java/com/example/zooseeker_team54/MockLocationTest.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Button;
 
+import androidx.lifecycle.Lifecycle;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -21,16 +22,20 @@ import java.util.concurrent.TimeUnit;
 @RunWith(AndroidJUnit4.class)
 public class MockLocationTest {
     Intent showDirectionIntent;
+    ActivityScenario<ShowDirectionActivity> showDirectionScneario;
 
     @Before
     public void setUp() {
         showDirectionIntent = new Intent(ApplicationProvider.getApplicationContext(), ShowDirectionActivity.class);
+        showDirectionScneario = ActivityScenario.launch(showDirectionIntent);
+        showDirectionScneario.moveToState(Lifecycle.State.CREATED);
+        showDirectionScneario.moveToState(Lifecycle.State.STARTED);
+        showDirectionScneario.moveToState(Lifecycle.State.RESUMED);
     }
 
     @Test
     public void simpleInputTest() {
-        ActivityScenario<ShowDirectionActivity> routeDirectionActivityActivityScenario = ActivityScenario.launch(showDirectionIntent);
-        routeDirectionActivityActivityScenario.onActivity(activity -> {
+        showDirectionScneario.onActivity(activity -> {
             activity.mockRouteInput.setText("89.55, 133.22");
             activity.mockStep.performClick();
 

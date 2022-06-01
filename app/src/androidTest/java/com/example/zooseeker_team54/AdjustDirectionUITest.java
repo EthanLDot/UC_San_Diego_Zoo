b45a1863f5b4f5e2dffda83b880b99d7249ad699
James Zhang <jjz002@ucsd.edu>
2022-05-31 21:48:42 -0700

    Add UI Test for US 3

---
+++ app/src/androidTest/java/com/example/zooseeker_team54/AdjustDirectionUITest.java
@@ -0,0 +1,231 @@
+package com.example.zooseeker_team54;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+// UI Test for MS 2 User Story 3
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AdjustDirectionUITest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void adjustDirectionUITest() {
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.clear_btn), withText("clear"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction materialAutoCompleteTextView = onView(
+                allOf(withId(R.id.search_bar),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialAutoCompleteTextView.perform(replaceText("g"), closeSoftKeyboard());
+
+        ViewInteraction materialTextView = onView(
+                allOf(withId(R.id.loc_name), withText("Gorillas"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.search_results),
+                                        1),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        ViewInteraction materialAutoCompleteTextView2 = onView(
+                allOf(withId(R.id.search_bar), withText("g"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialAutoCompleteTextView2.perform(replaceText("f"));
+
+        ViewInteraction materialAutoCompleteTextView3 = onView(
+                allOf(withId(R.id.search_bar), withText("f"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialAutoCompleteTextView3.perform(closeSoftKeyboard());
+
+        ViewInteraction materialTextView2 = onView(
+                allOf(withId(R.id.loc_name), withText("Fern Canyon"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.search_results),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView2.perform(click());
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.plan_btn), withText("plan"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        ViewInteraction materialButton3 = onView(
+                allOf(withId(R.id.direction_btn), withText("Direction"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton3.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.direction), withText("Proceed on 'Treetops Way' 1100 meters towards 'Treetops Way / Fern Canyon Trail' from 'Front Street / Treetops Way'.\n"),
+                        withParent(withParent(withId(R.id.route_direction))),
+                        isDisplayed()));
+        textView.check(matches(withText("Proceed on 'Treetops Way' 1100 meters towards 'Treetops Way / Fern Canyon Trail' from 'Front Street / Treetops Way'.\n")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.direction), withText("Proceed on 'Fern Canyon Trail' 6500 meters towards 'Fern Canyon' from 'Treetops Way / Fern Canyon Trail'.\n"),
+                        withParent(withParent(withId(R.id.route_direction))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Proceed on 'Fern Canyon Trail' 6500 meters towards 'Fern Canyon' from 'Treetops Way / Fern Canyon Trail'.\n")));
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.mock_route_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.mock_route_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("32.73585,-117.162894"), closeSoftKeyboard());
+
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.start_mock), withText("Mock"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        materialButton4.perform(click());
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.direction), withText("Proceed on 'Aviary Trail' 3000 meters towards 'Fern Canyon' from 'Parker Aviary'.\n"),
+                        withParent(withParent(withId(R.id.route_direction))),
+                        isDisplayed()));
+        textView3.check(matches(withText("Proceed on 'Aviary Trail' 3000 meters towards 'Fern Canyon' from 'Parker Aviary'.\n")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.direction), withText("Proceed on 'Orangutan Trail' 2600 meters towards 'Parker Aviary' from 'Siamangs'.\n"),
+                        withParent(withParent(withId(R.id.route_direction))),
+                        isDisplayed()));
+        textView4.check(matches(withText("Proceed on 'Orangutan Trail' 2600 meters towards 'Parker Aviary' from 'Siamangs'.\n")));
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(R.id.settings_button),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                7),
+                        isDisplayed()));
+        materialButton5.perform(click());
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(R.id.detailedDirectionsButton), withText("DETAILED"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton6.perform(click());
+
+        ViewInteraction materialButton7 = onView(
+                allOf(withId(R.id.exit_btn), withText("Exit"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton7.perform(click());
+
+        ViewInteraction textView5 = onView(
+                allOf(withId(R.id.direction), withText("Proceed on 'Aviary Trail' 1300 meters towards 'Owens Aviary' from 'Parker Aviary'.\n"),
+                        withParent(withParent(withId(R.id.route_direction))),
+                        isDisplayed()));
+        textView5.check(matches(withText("Proceed on 'Aviary Trail' 1300 meters towards 'Owens Aviary' from 'Parker Aviary'.\n")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
